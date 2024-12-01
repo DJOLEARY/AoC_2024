77e70aab20ad255b3097e0cef954b35ad66baf7d
DJ O'Leary <dijitol@proton.me>
2024-12-01 18:31:29 +0100

    feat: Setup direnv for NixOS

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+ # choose the ocaml version you want to use
+ ocamlPackages = pkgs.ocaml-ng.ocamlPackages_5_2;
+in
+pkgs.mkShell {
+  # build tools
+    nativeBuildInputs = with ocamlPackages; [ 
+      ocaml
+      findlib
+      dune_3
+      ocaml-lsp
+    ];
+  # dependencies
+    buildInputs = with ocamlPackages; [ 
+      bisect_ppx
+      findlib
+      menhir
+      ocaml-lsp
+      ocamlformat
+      ocamlgraph
+      odoc
+      ounit2
+      utop
+    ];
+}
