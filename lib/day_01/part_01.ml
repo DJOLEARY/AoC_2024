e5f65319fe6da7e42f309fd73d640ec29b6897a6
DJ O'Leary <dijitol@proton.me>
2024-12-01 18:34:00 +0100

    feat: Add solution for day 1 part 1

---
+++ lib/day_01/part_01.ml
@@ -0,0 +1,10 @@
+(* Day 1: Part 1 *)
+
+let calculate_distance x y = Stdlib.abs (x - y)
+
+(** Require: left and right to be the same length *)
+let solve left right =
+  let sorted_left = List.sort Stdlib.compare left in
+  let sorted_right = List.sort Stdlib.compare right in
+  let distances = List.map2 calculate_distance sorted_left sorted_right in
+  List.fold_left ( + ) 0 distances
---
+++ test/day_01_part_01.ml
@@ -0,0 +1,34 @@
+open OUnit2
+open AoC_2024.Day_01.Part_01
+
+let test_solve =
+  "test suite for day 01 part 01"
+  >::: [
+         ("empty list is 0" >:: fun _ -> assert_equal 0 (solve [] []));
+         ( "distance between 0 and 1 is 1" >:: fun _ ->
+           assert_equal 1 (solve [ 0 ] [ 1 ]) );
+         ( "distance between 1 and 0 is 1" >:: fun _ ->
+           assert_equal 1 (solve [ 1 ] [ 0 ]) );
+         ( "smallest are paired, then next smallest, ..." >:: fun _ ->
+           assert_equal 0 (solve [ 1; 2; 3 ] [ 3; 2; 1 ]) );
+         (* See https://adventofcode.com/2024/day/1 *)
+         ( "solves example correctly" >:: fun _ ->
+           assert_equal 11 (solve [ 3; 4; 2; 1; 3; 3 ] [ 4; 3; 5; 3; 9; 3 ]) );
+       ]
+
+let _ = run_test_tt_main test_solve
+
+let test_calculate_distance =
+  "test "
+  >::: [
+         ( "same number is 0 distance apart" >:: fun _ ->
+           assert_equal 0 (calculate_distance 0 0) );
+         ( "same number is 0 distance apart" >:: fun _ ->
+           assert_equal 0 (calculate_distance 1 1) );
+         ( "distance is equal to abs(x - y)" >:: fun _ ->
+           assert_equal 1 (calculate_distance 0 1) );
+         ( "order of inputs doesn't matter" >:: fun _ ->
+           assert_equal 1 (calculate_distance 1 0) );
+       ]
+
+let _ = run_test_tt_main test_calculate_distance
